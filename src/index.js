f02f740ffb0b8a814e30eb5a73deda4dca1a8dfe
Roberto Ochoa <oc_005@hotmail.com>
2023-05-18 14:37:04 -0300

    deploy front

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import axios from "axios";
 
 /* axios.defaults.baseURL = "http://localhost:3001"; */
-axios.defaults.baseURL = "https://countryappback-production.up.railway.app/";
+axios.defaults.baseURL = "https://countryappback-production.up.railway.app";
 
 ReactDOM.render(
   <Provider store={store}>
